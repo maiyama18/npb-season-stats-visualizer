e709b1ff7727e89844a40f9412dbda89467c8689
muiscript <myam0118@gmail.com>
2019-05-31 17:03:57 +0900

    stop to share db client among api server and crawl CLI

--- api/server/server.go
+++ api/server/server.go
@@ -2,11 +2,10 @@ package server
 
 import (
 	"fmt"
-	"net/http"
 	"os"
 	"strings"
 
-	"github.com/mui87/npb-season-stats-visualizer/db"
+	"github.com/mui87/npb-season-stats-visualizer/crawl/db"
 
 	"github.com/gin-gonic/gin"
 )
@@ -62,11 +61,11 @@ func (s *Server) Run() error {
 
 	// 検索用API: query(名前の一部と野手/投手)を受け取って、該当する選手一覧を返す
 	// 成績を返す用API: 選手のidを受け取って成績一覧を返す
-	api := r.Group("/api")
-	{
-		api.GET("/search/pitchers", s.searchPitchers)
-		api.GET("/search/batters", s.searchBatters)
-	}
+	//api := r.Group("/api")
+	//{
+	//api.GET("/search/pitchers", s.searchPitchers)
+	//api.GET("/search/batters", s.searchBatters)
+	//}
 
 	if err := r.Run(":" + s.port); err != nil {
 		return err
@@ -75,37 +74,37 @@ func (s *Server) Run() error {
 	return nil
 }
 
-func (s *Server) searchPitchers(c *gin.Context) {
-	query, ok := c.Request.URL.Query()["query"]
-	if !ok || query[0] == "" {
-		c.JSON(http.StatusOK, []db.Player{})
-		return
-	}
-
-	players, err := s.dbClient.SearchPitchers(query[0])
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to search pitchers: %s", err)})
-		return
-	}
-
-	c.JSON(http.StatusOK, players)
-}
-
-func (s *Server) searchBatters(c *gin.Context) {
-	query, ok := c.Request.URL.Query()["query"]
-	if !ok || query[0] == "" {
-		c.JSON(http.StatusOK, []db.Player{})
-		return
-	}
-
-	players, err := s.dbClient.SearchBatters(query[0])
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to search batters: %s", err)})
-		return
-	}
-
-	c.JSON(http.StatusOK, players)
-}
+//func (s *Server) searchPitchers(c *gin.Context) {
+//	query, ok := c.Request.URL.Query()["query"]
+//	if !ok || query[0] == "" {
+//		c.JSON(http.StatusOK, []db.Player{})
+//		return
+//	}
+//
+//	players, err := s.dbClient.SearchPitchers(query[0])
+//	if err != nil {
+//		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to search pitchers: %s", err)})
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, players)
+//}
+//
+//func (s *Server) searchBatters(c *gin.Context) {
+//	query, ok := c.Request.URL.Query()["query"]
+//	if !ok || query[0] == "" {
+//		c.JSON(http.StatusOK, []db.Player{})
+//		return
+//	}
+//
+//	players, err := s.dbClient.SearchBatters(query[0])
+//	if err != nil {
+//		c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("failed to search batters: %s", err)})
+//		return
+//	}
+//
+//	c.JSON(http.StatusOK, players)
+//}
 
 func getEnv(envName, defaultValue string) string {
 	value := os.Getenv(envName)
